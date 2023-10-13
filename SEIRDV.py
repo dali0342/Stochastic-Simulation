1433ccb72145d429857d5d0ee951e50371af4bc0
Ida Hellqvist <ida@pop-os.localdomain>
2023-10-13 21:16:05 +0200

    Polish up code and plotting

--- SEIRDV.py
+++ SEIRDV.py
@@ -11,11 +11,11 @@ D = 0 # number of dead individuals (at t0)
 V = 0 # number of vaccinated individuals (at t0)
 S = N-E-I # number of susceptible individuals (at t0)
 
-alpha = 1/5 # incubation time 
+alpha = 1/5.5 # incubation time 
 beta = 0.3 # proportion of susceptible individuals exposed to infection per unit of time
-gamma = 1/20 # proportion of sick individuals recovering per unit of time
-mu = 1/100 # proportion of sick individuals dying per unit of time (mortality rate)
-v = 0.05 # rate of vaccination
+gamma = 1/7 # proportion of sick individuals recovering per unit of time
+mu = 0.034 # proportion of sick individuals dying per unit of time (mortality rate)
+v = 0.80 # rate of vaccination
 phi = 0.0001 # proportion of vaccinated individuals transitioning to being susceptible per unit of time
 
 # stoichiometric matrix
@@ -53,17 +53,27 @@ t0 = 0
 t120 = 120
 tspan = [t0, t120]
 
-tvec, Xarr = gillespie.SSA(pro, sto, [S, E, I, R, D, V], tspan, [beta, alpha, gamma, mu, v, phi])
+num_simulations = 15
 
-plt.plot(tvec, Xarr[:, 0], label='Susceptible (S)')
-plt.plot(tvec, Xarr[:, 1], label='Exposed (E)')
-plt.plot(tvec, Xarr[:, 2], label='Infected (I)')
-plt.plot(tvec, Xarr[:, 3], label='Recovered (R)')
-plt.plot(tvec, Xarr[:, 4], label='Dead (D)')
-plt.plot(tvec, Xarr[:, 5], label='Vaccinated (V)')
+for i in range(num_simulations):
+    tvec, Xarr = gillespie.SSA(pro, sto, [S, E, I, R, D, V], tspan, [beta, alpha, gamma, mu, v, phi])
+    plt.plot(tvec, Xarr[:, 0], label='S: Susceptible', color='tab:blue')
+    plt.plot(tvec, Xarr[:, 1], label='E: Exposed', color='tab:purple')
+    plt.plot(tvec, Xarr[:, 2], label='I: Infected', color='tab:orange')
+    plt.plot(tvec, Xarr[:, 3], label='R: Recovered', color='tab:green')
+    plt.plot(tvec, Xarr[:, 4], label='D: Dead', color='tab:red')
+    plt.plot(tvec, Xarr[:, 5], label='V: Vaccinated', color='tab:pink')
+    i = 1 + i
 
-plt.legend()
-plt.title('SIERDV-model')
+# Only display 4 labels
+handles, labels = plt.gca().get_legend_handles_labels()
+by_label = dict(zip(labels, handles))
+plt.legend(by_label.values(), by_label.keys())
+
+plt.suptitle('SEIRDV-model', fontsize=16)
+plt.title('Vaccination rate = {}'.format(round(v*100,2)))
+
+
 plt.xlabel('Time')
 plt.ylabel('Number of Individuals')
 
