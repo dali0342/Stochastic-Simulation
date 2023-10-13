Correct faulty formula for vaccination rate

SEIRDV.py
@@ -4,29 +4,29 @@ import gillespie
 
 N = 1000 # size of population
 
-E = 2 # number of exposed individuals (at t0)
-I = 5 # number of infected individuals (at t0)
+E = 5 # number of exposed individuals (at t0)
+I = 0 # number of infected individuals (at t0)
 R = 0 # number of recovered individuals (at t0)
 D = 0 # number of dead individuals (at t0)
 V = 0 # number of vaccinated individuals (at t0)
 S = N-E-I # number of susceptible individuals (at t0)
 
-alpha = 1/5.5 # incubation time 
+alpha = 1/5 # incubation time 
 beta = 0.3 # proportion of susceptible individuals exposed to infection per unit of time
-gamma = 1/7 # proportion of sick individuals recovering per unit of time
-mu = alpha * gamma # proportion of sick individuals dying per unit of time (mortality rate)
-v = 0.001 # rate of vaccination
+gamma = 1/20 # proportion of sick individuals recovering per unit of time
+mu = 1/100 # proportion of sick individuals dying per unit of time (mortality rate)
+v = 0.05 # rate of vaccination
 phi = 0.0001 # proportion of vaccinated individuals transitioning to being susceptible per unit of time
 
 # stoichiometric matrix
 def sto():
     return np.array([
-        [ -1,  1,  0,  0,  0,  0], 
-        [  0, -1,  1,  0,  0,  0], 
-        [  0,  0, -1,  1,  0,  0],
-        [  0,  0, -1,  0,  1,  0],
-        [ -1,  0,  0,  0,  0,  1],
-        [  1,  0,  0,  0,  0, -1]
+        [-1,1,0,0,0,0], 
+        [0,-1,1,0,0,0], 
+        [0,0,-1,1,0,0],
+        [0,0,-1,0,1,0],
+        [-1,0,0,0,0,1]
+        #[1,0,0,0,0,-1]
         ])
 
 # propensity function
@@ -37,8 +37,8 @@ def pro(values, coeff):
     propensity_infected = alpha * E
     propensity_recovered = gamma * I
     propensity_dead = mu * I
-    propensity_vaccinated = v * S
-    propensity_vaccinated_to_susceptible = phi * V
+    propensity_vaccinated = v
+    #propensity_vaccinated_to_susceptible = phi * V
     
     return np.array([
         propensity_exposed, 
@@ -46,7 +46,7 @@ def pro(values, coeff):
         propensity_recovered, 
         propensity_dead,
         propensity_vaccinated,
-        propensity_vaccinated_to_susceptible
+        #propensity_vaccinated_to_susceptible
         ])
 
 t0 = 0
@@ -56,14 +56,14 @@ tspan = [t0, t120]
 tvec, Xarr = gillespie.SSA(pro, sto, [S, E, I, R, D, V], tspan, [beta, alpha, gamma, mu, v, phi])
 
 plt.plot(tvec, Xarr[:, 0], label='Susceptible (S)')
-plt.plot(tvec, Xarr[:, 1], label='Exposed      (E)')
-plt.plot(tvec, Xarr[:, 2], label='Infected       (I)')
-plt.plot(tvec, Xarr[:, 3], label='Recovered   (R)')
-plt.plot(tvec, Xarr[:, 4], label='Dead           (D)')
-plt.plot(tvec, Xarr[:, 5], label='Vaccinated  (V)')
+plt.plot(tvec, Xarr[:, 1], label='Exposed (E)')
+plt.plot(tvec, Xarr[:, 2], label='Infected (I)')
+plt.plot(tvec, Xarr[:, 3], label='Recovered (R)')
+plt.plot(tvec, Xarr[:, 4], label='Dead (D)')
+plt.plot(tvec, Xarr[:, 5], label='Vaccinated (V)')
 
 plt.legend()
-plt.title('SEIRDV-model')
+plt.title('SIERDV-model')
 plt.xlabel('Time')
 plt.ylabel('Number of Individuals')
 