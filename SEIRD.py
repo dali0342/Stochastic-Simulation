7c8ab8b4259723e5804627dfabd2e10dba10a329
Ida Hellqvist <ida@pop-os.localdomain>
2023-10-13 20:43:39 +0200

    Polish up code and plotting

--- SEIRD.py
+++ SEIRD.py
@@ -13,7 +13,7 @@ S = N-E-I # number of susceptible individuals (at t0)
 alpha = 1/5.5      # incubation time 
 beta = 0.3         # proportion of susceptible individuals exposed to infection per unit of time
 gamma = 1/7        # proportion of sick individuals recovering per unit of time
-mu = alpha * gamma # proportion of sick individuals dying per unit of time (mortality rate)
+mu = 0.034          # proportion of sick individuals dying per unit of time (mortality rate)
 
 # stoichiometric matrix
 def sto():
@@ -44,16 +44,26 @@ t0 = 0
 t120 = 120
 tspan = [t0, t120]
 
-tvec, Xarr = gillespie.SSA(pro, sto, [S, E, I, R, D], tspan, [beta, alpha, gamma, mu])
+num_simulations = 15
 
-plt.plot(tvec, Xarr[:, 0], label='Susceptible (S)')
-plt.plot(tvec, Xarr[:, 1], label='Exposed      (E)')
-plt.plot(tvec, Xarr[:, 2], label='Infected       (I)')
-plt.plot(tvec, Xarr[:, 3], label='Recovered   (R)')
-plt.plot(tvec, Xarr[:, 4], label='Dead           (D)')
+for i in range(num_simulations):
+    tvec, Xarr = gillespie.SSA(pro, sto, [S, E, I, R, D], tspan, [beta, alpha, gamma, mu])
 
-plt.legend()
-plt.title('SEIRD-model')
+    plt.plot(tvec, Xarr[:, 0], label='S: Susceptible', color='tab:blue')
+    plt.plot(tvec, Xarr[:, 1], label='E: Exposed', color='tab:purple')
+    plt.plot(tvec, Xarr[:, 2], label='I: Infected', color='tab:orange')
+    plt.plot(tvec, Xarr[:, 3], label='R: Recovered', color='tab:green')
+    plt.plot(tvec, Xarr[:, 4], label='D: Dead', color='tab:red')
+    i = 1 + i
+
+# Only display 4 labels
+handles, labels = plt.gca().get_legend_handles_labels()
+by_label = dict(zip(labels, handles))
+plt.legend(by_label.values(), by_label.keys())
+
+plt.suptitle('SEIRD-model', fontsize=16)
+plt.title('Mortality rate = {}'.format(round(mu*100,2)))
+
 plt.xlabel('Time')
 plt.ylabel('Number of Individuals')
 
